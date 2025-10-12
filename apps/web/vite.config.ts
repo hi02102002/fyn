eec5171f30451d10e25513913999b7dfc178a35d
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2025-10-12 17:28:49 +0700

    feat: add build-report task and update dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,13 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
 	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+	build: {
+		rollupOptions: {
+			plugins: [visualizer()],
+		},
+	}
 });
