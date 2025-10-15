cd03f54a5b3e4c6395c0cf00f67ebd3335e6f7bc
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2025-10-15 23:26:56 +0700

    feat: setup ci to check lint and type-check (#1)
    * feat: add check-types script to package.json files and configure CI workflow
    
    * fix: remove duplicate import of visualizer in vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
 	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
@@ -11,5 +11,5 @@ export default defineConfig({
 		rollupOptions: {
 			plugins: [visualizer()],
 		},
-	}
+	},
 });
