0c8fbfa8d9f938b422293b0590fb2da4ec59608c
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2025-10-23 22:55:28 +0700

    wip

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [
+		tsconfigPaths(),
+		tailwindcss(),
+		tanstackStart(),
+		nitro(
+			// nitro config goes here, e.g.
+			{ config: { preset: "node-server" } },
+		),
+		viteReact(),
+	],
 	build: {
 		rollupOptions: {
 			plugins: [visualizer()],
